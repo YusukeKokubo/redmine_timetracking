ca69228a7ffe3a0ee102ce2ad3f0da5e792f6c6c
ykokubo <ykokubo@ykokubo-virtualbox.otr-jp.com>
2012-06-26 17:52:18 +0900

    Redmine 2.0 compatible

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_timetracking_plugin do
-  name 'Redmine Timetracking Plugin plugin'
+Redmine::Plugin.register :redmine_timetracking do
+  name 'Redmine Timetracking Plugin'
   author 'Yusuke Kokubo'
   description 'This is a plugin for Redmine to show time tracking.'
   version '0.0.1'
